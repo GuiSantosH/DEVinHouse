265798cb5748feda32e7cad7ccbd5a356890026a
unknown <guiholm72@gmail.com>
2022-07-14 20:08:37 -0300

    Ex 2 - Iterando sobre arrays de objetos

--- semana3/script.js
+++ semana3/script.js
@@ -1,8 +1,27 @@
-const listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78, 17 ]
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+  ];
 
 
-listaNumeros.forEach((listaNumeros) => {
-    console.log(listaNumeros*2);
-    
+let lista = []
+
+listaConvidados.forEach(name => {
+  var itens = name.nome + ' ' + name.sobrenome
+  lista.push(itens)
 })
 
+for(i in lista) {
+  let ul = document.getElementById('nomes')
+  let li = document.createElement('li')
+  li.appendChild(document.createTextNode(lista[i]))
+  ul.appendChild(li)
+}
