8a45e60242b34820030b003822cfcb99b41cb01e
unknown <guiholm72@gmail.com>
2022-07-19 20:56:02 -0300

    Ex 6 - Projeto: funções e transformação de objetos

--- semana3/scriptpessoas.js
+++ semana3/scriptpessoas.js
@@ -7,18 +7,32 @@ var representaPessoa = (nome, sobrenome, setor, idade) => {
     };
   };
 
-  var pUm = representaPessoa('Guilherme', 'Santos', 'camarote', 18);
-  var pDois = representaPessoa('Flavio', 'Silva', 'camarote', 21);
-  var pTres = representaPessoa('Lorenz', 'Pontes', 'pista', 21);
-  var pQuatro = representaPessoa('Luan', 'Rodrigo', 'pista', 23);
-  var pCinco = representaPessoa('Diego', 'Almeida', 'arquibancada', 15);
-  var pSeis = representaPessoa('Leandro', 'Oliveira', 'arquibancada', 19);
+  var pessoa1 = representaPessoa('Guilherme', 'Santos', 'camarote', 18);
+  var pessoa2 = representaPessoa('Flavio', 'Silva', 'camarote', 21);
+  var pessoa3 = representaPessoa('Lorenz', 'Pontes', 'pista', 21);
+  var pessoa4 = representaPessoa('Luan', 'Rodrigo', 'pista', 23);
+  var pessoa5 = representaPessoa('Diego', 'Almeida', 'arquibancada', 15);
+  var pessoa6 = representaPessoa('Leandro', 'Oliveira', 'arquibancada', 19);
 
- var listaDeConvidados = [
-    pUm,
-    pDois,
-    pTres,
-    pQuatro,
-    pCinco,
-    pSeis
+ const listaDeConvidados = [
+    pessoa1,
+    pessoa2,
+    pessoa3,
+    pessoa4,
+    pessoa5,
+    pessoa6,
  ];
+
+function liberarBebidas(lista) {
+  var resultado = lista.map((pessoa) => {
+    pessoa.openBar = false;
+    if (pessoa.idade >= 18) {
+      pessoa.openBar = true;
+    }
+    return pessoa;
+  });
+return resultado;
+}
+
+
+const convidadosComBebidasProcessada = liberarBebidas(listaDeConvidados);
