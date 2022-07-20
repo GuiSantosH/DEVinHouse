d10bf927df333f657035b88b38fee38c09621138
unknown <guiholm72@gmail.com>
2022-07-19 23:55:12 -0300

    Ex 7 e 8

--- semana3/scriptpessoas.js
+++ semana3/scriptpessoas.js
@@ -35,4 +35,63 @@ return resultado;
 }
 
 
+var separarCamarote = (lista) => {
+  var resultado = lista.filter((pessoa) => {
+    if (pessoa.setor === "camarote") {
+      return true;
+    } else {
+      return false; 
+    }
+  });
+  return resultado;
+};
+
+
+var separarPista = (lista) => {
+  var resultado = lista.filter((pessoa) => {
+    if (pessoa.setor === "pista") {
+      return true;
+    } else {
+      return false; 
+    }
+  });
+  return resultado; 
+};
+
+var separarArquibancada = (lista) => {
+  var resultado = lista.filter((pessoa) => {
+    if (pessoa.setor === "arquibancada") {
+      return true;
+    } else {
+      return false; 
+    }
+  });
+  return resultado; 
+};
+
+const filtrarPorSetor = (lista, setor) => {
+
+  return lista.filter((conv) => conv.setor === setor);
+};
+
 const convidadosComBebidasProcessada = liberarBebidas(listaDeConvidados);
+
+const listaArquibancada = separarArquibancada(convidadosComBebidasProcessada);
+const listaCamarote = separarCamarote(convidadosComBebidasProcessada);
+const listaPista = separarPista(convidadosComBebidasProcessada);
+
+const ulArquibancada = document.getElementById("arquibancada");
+const ulPista = document.getElementById("pista");
+const ulCamarote = document.getElementById("camarote");
+
+listaArquibancada.forEach((pessoa) => {
+  ulArquibancada.innerHTML += `<li>${pessoa.nome} `+` ${pessoa.sobrenome}</li>`;
+});
+
+listaCamarote.forEach((pessoa) => {
+  ulCamarote.innerHTML += `<li>${pessoa.nome} `+` ${pessoa.sobrenome}</li>`;
+});
+
+listaPista.forEach((pessoa) => {
+  ulPista.innerHTML += `<li>${pessoa.nome} `+` ${pessoa.sobrenome}</li>`;
+});
