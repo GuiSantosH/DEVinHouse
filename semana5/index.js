76a03e4e69a6e2b6ec23cd15b286e87fa7b99931
unknown <guiholm72@gmail.com>
2022-07-31 08:43:01 -0300

    Ex 2 - Criando módulos

--- semana5/index.js
+++ semana5/index.js
@@ -1,4 +1,6 @@
 import {saudacao} from './saudacao.js' //EX 1
+import saudacaoEspecial from './saudacao-especial.js' //Exercicio 2
 
 console.log(saudacao()); //Exercicio 1
 
+console.log(saudacaoEspecial("Guilherme")); //Exercicio 2
---
+++ semana5/saudacao-especial.js
@@ -0,0 +1,7 @@
+//Exercicio 2
+
+function saudacaoEspecial(nome){
+    return `Olá ${nome}! Seja muito bem vindo!`
+}
+
+export default saudacaoEspecial;
